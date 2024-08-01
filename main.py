d163c7f0ca5d11cea8258d8bbbdcab55b86dd0ef
ShoaibVai <omar22205101393@diu.edu.bd>
2024-08-01 13:07:12 +0600

    main

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import random
 import os
 
+HEAD = 1
+TAIL = 2
+BOWLING = 1
+BATTING = 2
+
 def crik(bowler, batter):
     if bowler == batter:
         return -1
@@ -10,158 +15,116 @@ def crik(bowler, batter):
         return 0
 
 def printToss(comToss):
-    if comToss == 1:
+    if comToss == HEAD:
         print("Coin Landed on HEAD!!!\n")
-    if comToss == 2:
+    elif comToss == TAIL:
         print("Coin Landed on TAIL!!!\n")
 
-def BowlingWtarget(wicket, over, target):
+def play_innings(role, wicket, over, target=None):
     score = 0
-    wicketCount = 0
-    
+    wicket_count = 0
     for ball in range(over * 6):
-        if score > target:
+        if target and score > target:
             break
-        comBat = random.randint(1, 6)
-        plyrBall = int(input(f"Ball {ball + 1} Total Run {score}\nYou Choose for Bowling: "))
-        print(f"Computer Used {comBat}")
-        if crik(plyrBall, comBat) == -1:
-            wicketCount += 1
-            if wicketCount == wicket:
+        comp_choice = random.randint(1, 6)
+        try:
+            user_choice = int(input(f"Ball {ball + 1} Total Run {score}\nYou Choose for {'Bowling' if role == BOWLING else 'Batting'}: "))
+        except ValueError:
+            print("Invalid input, please enter a number between 1 and 6.")
+            continue
+        print(f"Computer Used {comp_choice}")
+        result = crik(user_choice, comp_choice) if role == BOWLING else crik(comp_choice, user_choice)
+        if result == -1:
+            wicket_count += 1
+            if wicket_count == wicket:
                 print("ALL OUT!!!!!!!")
                 break
-        elif 1 <= crik(plyrBall, comBat) <= 6:
-            score += crik(plyrBall, comBat)
+        elif 1 <= result <= 6:
+            score += result
         else:
             print("FOUL INPUT")
     return score
 
-def Bowling(wicket, over):
-    score = 0
-    wicketCount = 0
-    
-    for ball in range(over * 6):
-        comBat = random.randint(1, 6)
-        plyrBall = int(input(f"Ball {ball + 1} Total Run {score}\nYou Choose for Bowling: "))
-        print(f"Computer Used {comBat}")
-        if crik(plyrBall, comBat) == -1:
-            wicketCount += 1
-            if wicketCount == wicket:
-                print("ALL OUT!!!!!!!")
-                break
-        elif 1 <= crik(plyrBall, comBat) <= 6:
-            score += crik(plyrBall, comBat)
-        else:
-            print("FOUL INPUT")
-    return score
+def main():
+    try:
+        over = int(input("How many over(s) do you want to play?:\n"))
+        wicket = int(input("How many wicket(s) do you want to play?:\n"))
+    except ValueError:
+        print("Invalid input, please enter a valid number.")
+        return
 
-def BattingWtarget(wicket, over, target):
-    score = 0
-    wicketCount = 0
-    for ball in range(over * 6):
-        if score > target:
-            break
-        comBall = random.randint(1, 6)
-        plyrBat = int(input(f"Ball {ball + 1} Total Run {score}\nYou Choose for Batting: "))
-        print(f"Computer Used {comBall}")
-        if crik(comBall, plyrBat) == -1:
-            wicketCount += 1
-            if wicketCount == wicket:
-                print("ALL OUT!!!")
-                break
-        elif 1 <= crik(comBall, plyrBat) <= 6:
-            score += crik(comBall, plyrBat)
-        else:
-            print("FOUL INPUT")
-    return score
-
-def Batting(wicket, over):
-    score = 0
-    wicketCount = 0
-    for ball in range(over * 6):
-        comBall = random.randint(1, 6)
-        plyrBat = int(input(f"Ball {ball + 1} Total Run {score}\nYou Choose for Batting: "))
-        print(f"Computer Used {comBall}")
-        if crik(comBall, plyrBat) == -1:
-            wicketCount += 1
-            if wicketCount == wicket:
-                print("ALL OUT!!!")
-                break
-        elif 1 <= crik(comBall, plyrBat) <= 6:
-            score += crik(comBall, plyrBat)
-        else:
-            print("FOUL INPUT")
-    return score
-
-def play():
-    over = int(input("How many over(s) do you want to play?:\n"))
-    wicket = int(input("How many wicket(s) do you want to play?:\n"))
     comToss = random.randint(1, 2)
-    toss = int(input("1. HEAD! or 2.TAIL!!\n"))
+    try:
+        toss = int(input("1. HEAD! or 2.TAIL!!\n"))
+    except ValueError:
+        print("Invalid input, please enter 1 or 2.")
+        return
+
     if toss == comToss:
-        # player Won the toss
         printToss(comToss)
-        tossChoice = int(input("You Won The toss!!!\n1. Bowling or 2. Batting\n"))
-        if tossChoice == 1:  # player chose to bowl
-            bowlingResult = Bowling(wicket, over)
-            print(f"!!!   Player Needs {bowlingResult + 1} Run(s) to win  !!!")
+        try:
+            tossChoice = int(input("You Won The toss!!!\n1. Bowling or 2. Batting\n"))
+        except ValueError:
+            print("Invalid input, please enter 1 or 2.")
+            return
+
+        if tossChoice == BOWLING:
+            target = play_innings(BOWLING, wicket, over)
+            print(f"!!!   Player Needs {target + 1} Run(s) to win  !!!")
             input("Press \"Enter\" to continue")
             os.system("cls" if os.name == "nt" else "clear")
-            print(f"!!!   Player Needs {bowlingResult + 1} Run(s) to win  !!!")
-            battingResult = BattingWtarget(wicket, over, bowlingResult)
-            if bowlingResult < battingResult:
+            print(f"!!!   Player Needs {target + 1} Run(s) to win  !!!")
+            score = play_innings(BATTING, wicket, over, target)
+            if score > target:
+                print("YOU WON THE MATCH!!!!!!!!!!!!!")
+            else:
                 print("YOU LOST THE MATCH!")
-            elif bowlingResult > battingResult:
-                print("YOU WON THE MATCH!!!!!!!!!!!!!")
-        elif tossChoice == 2:  # player chose to bat
-            battingResult = Batting(wicket, over)
-            print(f"!!!   Computer Needs {battingResult + 1} Run(s) to win  !!!")
+        elif tossChoice == BATTING:
+            score = play_innings(BATTING, wicket, over)
+            print(f"!!!   Computer Needs {score + 1} Run(s) to win  !!!")
             input("Press \"Enter\" to continue")
             os.system("cls" if os.name == "nt" else "clear")
-            print(f"!!!   Computer Needs {battingResult + 1} Run(s) to win  !!!")
-            bowlingResult = BowlingWtarget(wicket, over, battingResult)
-            if battingResult > bowlingResult:
+            print(f"!!!   Computer Needs {score + 1} Run(s) to win  !!!")
+            target = play_innings(BOWLING, wicket, over, score)
+            if target < score:
                 print("YOU WON THE MATCH!!!!!!!!!!!!!")
-            elif bowlingResult > battingResult:
+            else:
                 print("YOU LOST THE MATCH!")
         else:
             print("INVALID INPUT")
-    # if computer wins the toss        
     else:
         printToss(comToss)
         print("You lost the Toss")
         comChoice = random.randint(1, 2)
-        if comChoice == 1:
+        if comChoice == BOWLING:
             print("Computer Chose to Bowl")
-        elif comChoice == 2:
+            score = play_innings(BATTING, wicket, over)
+            print(f"!!!   Computer Needs {score + 1} Run(s) to win  !!!")
+            input("Press \"Enter\" to continue")
+            os.system("cls" if os.name == "nt" else "clear")
+            print(f"!!!   Computer Needs {score + 1} Run(s) to win  !!!")
+            target = play_innings(BOWLING, wicket, over, score)
+            if target < score:
+                print("YOU WON THE MATCH!!!!!!!!!!!!!")
+            else:
+                print("YOU LOST THE MATCH!")
+        elif comChoice == BATTING:
             print("Computer Chose to Bat")
-
-        if comChoice == 1:
-            battingResult = Batting(wicket, over)
-            print(f"!!!   Computer Needs {battingResult + 1} Run(s) to win  !!!")
+            target = play_innings(BOWLING, wicket, over)
+            print(f"!!!   Player Needs {target + 1} Run(s) to win  !!!")
             input("Press \"Enter\" to continue")
             os.system("cls" if os.name == "nt" else "clear")
-            print(f"!!!   Computer Needs {battingResult + 1} Run(s) to win  !!!")
-            bowlingResult = BowlingWtarget(wicket, over, battingResult)
-            if battingResult > bowlingResult:
+            print(f"!!!   Player Needs {target + 1} Run(s) to win  !!!")
+            score = play_innings(BATTING, wicket, over, target)
+            if score > target:
                 print("YOU WON THE MATCH!!!!!!!!!!!!!")
-            elif bowlingResult > battingResult:
+            else:
                 print("YOU LOST THE MATCH!")
-        elif comChoice == 2:
-            bowlingResult = Bowling(wicket, over)
-            print(f"!!!   Player Needs {bowlingResult + 1} Run(s) to win  !!!")
-            input("Press \"Enter\" to continue")
-            os.system("cls" if os.name == "nt" else "clear")
-            print(f"!!!   Player Needs {bowlingResult + 1} Run(s) to win  !!!")
-            battingResult = BattingWtarget(wicket, over, bowlingResult)
-            if bowlingResult < battingResult:
-                print("YOU LOST THE MATCH!")
-            elif bowlingResult > battingResult:
-                print("YOU WON THE MATCH!!!!!!!!!!!!!")
         else:
             print("INVALID INPUT")
-    r = input("Press \"Enter\" to continue or 'R' to Replay the Game")
+    r = input("Press \"Enter\" to continue or 'R' to Replay the Game\n")
     if r.lower() == "r":
-        play()
+        main()
 
-play()
+if __name__ == "__main__":
+    main()
